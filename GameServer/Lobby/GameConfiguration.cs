3aef89f361228c3c2d426ad527bc34e74e601a3a
Michitaro Naito <m-naito@amlitek.com>
2014-04-17 12:38:08 +0900

    GameConfiguration, App.config wrapper

---
+++ GameServer/Lobby/GameConfiguration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer {
+    public static class GameConfiguration {
+
+        public static string Host {
+            get { return ConfigurationManager.AppSettings["Host"]; }
+        }
+
+        public static string Port {
+            get { return ConfigurationManager.AppSettings["Port"]; }
+        }
+
+        public static string Name {
+            get { return ConfigurationManager.AppSettings["Name"]; }
+        }
+
+        public static string ListenUrl {
+            get { return string.Format("http://{0}:{1}", Host, Port); }
+        }
+
+        public static bool HasError {
+            get {
+                return Host == null
+                    || Port == null
+                    || Name == null;
+            }
+        }
+
+        public static string ToString() {
+            if (HasError)
+                return string.Format("[***ERROR*** LobbyConfiguration Host:{0} Port:{1} Name:{2}]", Host, Name);
+            return string.Format("[LobbyConfiguration Host:{0} Port:{1} Name:{2}]", Host, Port, Name);
+        }
+    }
+}
--- GameServer/Program.cs
+++ GameServer/Program.cs
@@ -4,18 +4,23 @@ using Microsoft.Owin.Hosting;
 using Owin;
 using Microsoft.Owin.Cors;
 using System.Threading;
+using System.Net;
+using System.Configuration;
 
 namespace GameServer
 {
     class Program
     {
+        // GameServer.exe [Debug/Release] [host(192.168.100.3)] [name(Tokyo#1)]
         static void Main(string[] args)
         {
             // This will *ONLY* bind to localhost, if you want to bind to all addresses
             // use http://*:8080 to bind to all addresses. 
             // See http://msdn.microsoft.com/en-us/library/system.net.httplistener.aspx 
             // for more information.
-            string url = "http://*:8080";
+            Console.WriteLine(GameConfiguration.ToString());
+
+            string url = GameConfiguration.ListenUrl;   //"http://*:8080";
             var t = new Thread(new ThreadStart(() =>
             {
                 while(true){
